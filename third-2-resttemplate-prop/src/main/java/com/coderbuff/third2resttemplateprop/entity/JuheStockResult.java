6171c1b0d556e2abe0bc92e4881fbacf86f81299
yuke <627617510@qq.com>
2021-01-09 11:49:25 +0800

    第三方接口调用demo

---
+++ third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/entity/JuheStockResult.java
@@ -0,0 +1,16 @@
+package com.coderbuff.third2resttemplateprop.entity;
+
+import lombok.Data;
+
+/**
+ * @author yulinfeng
+ * @date 2019/12/26
+ */
+@Data
+public class JuheStockResult {
+
+    private JuheStockResultDapanData dapandata;
+
+    private GoPicture gopicture;
+
+}
---
+++ third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/entity/JuheStockResultDapanData.java
@@ -0,0 +1,17 @@
+package com.coderbuff.third2resttemplateprop.entity;
+
+import lombok.Data;
+
+/**
+ * @author yulinfeng
+ * @date 2019/12/26
+ */
+@Data
+public class JuheStockResultDapanData {
+    private String name;
+    private String dot;
+    private String nowPic;
+    private String rate;
+    private String traAmount;
+    private String traNumber;
+}
