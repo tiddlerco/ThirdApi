43d0f4d339310fad11762ac12bd05452fe1fc13b
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2022-05-11 14:07:39 +0800

    InstantiationAwareBeanPostProcessor执行流程解析

---
+++ third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/study/postprocessor/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,30 @@
+package com.coderbuff.third2resttemplateprop.study.postprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author 喻可
+ * @Date 2022/5/11 13:57
+ */
+@Component
+public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】开始");
+
+        return null;
+    }
+
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+
+        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】开始");
+
+        return true;
+    }
+}
