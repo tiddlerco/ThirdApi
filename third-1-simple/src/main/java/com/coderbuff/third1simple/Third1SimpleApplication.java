a35394d736e151143be5b328704c7405a619f5ab
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2022-05-18 18:17:42 +0800

    nacos配置同步到sentinel,保证sentinel的配置持久化

--- third-1-simple/src/main/java/com/coderbuff/third1simple/Third1SimpleApplication.java
+++ third-1-simple/src/main/java/com/coderbuff/third1simple/Third1SimpleApplication.java
@@ -2,8 +2,9 @@ package com.coderbuff.third1simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Third1SimpleApplication {
 
