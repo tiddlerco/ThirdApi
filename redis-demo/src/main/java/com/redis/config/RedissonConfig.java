ae11c840e6f8e2997a1458472c4ff6b9661afbd6
yuke <627617510@qq.com>
2022-07-25 14:44:12 +0800

    update code

--- redis-demo/src/main/java/com/redis/config/RedissonConfig.java
+++ redis-demo/src/main/java/com/redis/config/RedissonConfig.java
@@ -3,17 +3,20 @@ package com.redis.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.host}")
+    private String address;
 
     @Bean(destroyMethod = "shutdown")
     RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.useSingleServer().setAddress("redis://"+address+":6379");
         return Redisson.create(config);
     }
 
