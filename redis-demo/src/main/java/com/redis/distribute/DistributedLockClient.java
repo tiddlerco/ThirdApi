0c91413f1f2809a8e62365db09921fec1cb0607a
tiddler <627617510@qq.com>
2022-11-07 23:03:12 +0800

    redis+lua实现可重入、自动续期的分布式锁

---
+++ redis-demo/src/main/java/com/redis/distribute/DistributedLockClient.java
@@ -0,0 +1,34 @@
+package com.redis.distribute;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * @title: 分布式锁客户端  工厂类
+ * @Author yuke
+ * @Date: 2022/11/7 21:19
+ */
+@Component
+public class DistributedLockClient {
+
+    @Resource
+    private StringRedisTemplate redisTemplate;
+
+    private String uuid;
+
+    //@Component会默认调用对象的无参构造器
+    public DistributedLockClient() {
+        this.uuid = UUID.randomUUID().toString();
+        // 这个UUID在spring容器初始化时就已经创建好了，保证了一个服务器的uuid都是一样的
+        // 同一个服务中用ThreadId区分
+        // 不同服务中首先uuid就不同了，就区分开了
+    }
+
+    public DistributedRedisLock getRedisLock(String lockName) {
+        return new DistributedRedisLock(redisTemplate, lockName, uuid);
+    }
+}
---
+++ redis-demo/src/main/java/com/redis/distribute/DistributedRedisLock.java
@@ -0,0 +1,118 @@
+package com.redis.distribute;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @title: DistributedRedisLock
+ * @Author yuke
+ * @Date: 2022/11/7 21:19
+ */
+public class DistributedRedisLock implements Lock {
+
+    private StringRedisTemplate redisTemplate;
+
+    private String lockName;
+
+    private String uuid;
+
+    private long expire = 30;  // 默认过期时间30ms
+
+    public DistributedRedisLock(StringRedisTemplate redisTemplate, String lockName, String uuid) {
+        this.redisTemplate = redisTemplate;
+        this.lockName = lockName;
+        this.uuid = uuid + ":" + Thread.currentThread().getId();  // 这步关键
+    }
+
+    @Override
+    public void lock() {
+        this.tryLock();
+    }
+
+
+    @Override
+    public boolean tryLock() {
+        try {
+            return this.tryLock(-1L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        if (time != -1) {
+            this.expire = unit.toSeconds(time);
+        }
+        // 还未上锁，已经上锁且是自己的
+        String script = "if redis.call('exists', KEYS[1]) == 0 or redis.call('hexists', KEYS[1], ARGV[1]) == 1 " +
+                "then " +
+                "   redis.call('hincrby', KEYS[1], ARGV[1], 1) " +
+                "   redis.call('expire', KEYS[1], ARGV[2]) " +
+                "   return 1 " +
+                "else " +
+                "   return 0 " +
+                "end";
+        while (!this.redisTemplate.execute(new DefaultRedisScript<>(script, Boolean.class), Arrays.asList(lockName), uuid, String.valueOf(expire))) {
+            Thread.sleep(50);
+        }
+        // 加锁成功，返回之前，开启定时器自动续期
+        this.renewExpire();
+        return true;
+    }
+
+    @Override
+    public void unlock() {
+        String script = "if redis.call('hexists', KEYS[1], ARGV[1]) == 0 " +
+                "then " +
+                "   return nil " +
+                "elseif redis.call('hincrby', KEYS[1], ARGV[1], -1) == 0 " +
+                "then " +
+                "   return redis.call('del', KEYS[1]) " +
+                "else " +
+                "   return 0 " +
+                "end";
+        Long flag = this.redisTemplate.execute(new DefaultRedisScript<>(script, Long.class), Arrays.asList(lockName), uuid);
+        if (flag == null) {
+            throw new IllegalMonitorStateException("this lock doesn't belong to you!");
+        }
+    }
+
+
+    private void renewExpire() {
+        String script = "if redis.call('hexists', KEYS[1], ARGV[1]) == 1 " +
+                "then " +
+                "   return redis.call('expire', KEYS[1], ARGV[2]) " +
+                "else " +
+                "   return 0 " +
+                "end";
+
+        //定时器 用于锁续期
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {  // 开启的定时器是一个子线程，不能用上面的getId方法，而是直接在主线程中拼好uuid（该uuid是生成的uuid+线程数），然后这里用
+                if (redisTemplate.execute(new DefaultRedisScript<>(script, Boolean.class), Arrays.asList(lockName), uuid, String.valueOf(expire))) {
+                    renewExpire();  // 再次开启一次性任务，直到上面续期失败了就直接结束了
+                }  // 另一种实现方式是用周期任务，在unlock中取消定时任务
+            }
+        }, this.expire * 1000 / 3);// 设置成只有延迟时间，是一次性的任务，就不需要思考结束任务的方式
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
