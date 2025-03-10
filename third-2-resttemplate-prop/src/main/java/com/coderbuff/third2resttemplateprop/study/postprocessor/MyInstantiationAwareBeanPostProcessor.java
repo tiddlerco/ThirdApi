9d9e6961362174360dd3a056ceb8bc302459d684
yuke <627617510yuke@gmail.com>
2025-03-10 15:34:10 +0800

    修改bean生命周期的注释

--- third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/study/postprocessor/MyInstantiationAwareBeanPostProcessor.java
+++ third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/study/postprocessor/MyInstantiationAwareBeanPostProcessor.java
@@ -1,37 +1,38 @@
-//package com.coderbuff.third2resttemplateprop.study.postprocessor;
-//
-//import org.springframework.beans.BeansException;
-//import org.springframework.beans.PropertyValues;
-//import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @Author 喻可
-// * @Date 2022/5/11 13:57
-// */
-//@Component
-//public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
-//
-//
-//    @Override
-//    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
-//        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】开始");
-//
-//        return null;
-//    }
-//
-//
-//    @Override
-//    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-//
-//        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】结束");
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
-//        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理执行postProcessProperties方法");
-//        return null;
-//    }
-//}
+package com.coderbuff.third2resttemplateprop.study.postprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author 喻可
+ * @Date 2022/5/11 13:57
+ * 影响多个bean
+ */
+@Component
+public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】开始");
+
+        return null;
+    }
+
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+
+        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理bean=【" + beanName + "】结束");
+
+        return true;
+    }
+
+    @Override
+    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        System.out.println("[MyInstantiationAwareBeanPostProcessor]后置处理器处理执行postProcessProperties方法");
+        return null;
+    }
+}
